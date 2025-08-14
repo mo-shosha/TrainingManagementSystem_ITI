e4ffe637a972e613682b8bda8096fc3d3d0d992b
Mohamed_Shosha <shoshamohamed313@gmail.com>
2025-08-14 13:24:28 +0300

    Add project files.

---
+++ TrainingManagementSystem_ITI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrainingManagementSystem_ITI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
